41fd567f60ad32a4603a01d62c345f408bb62009
AimPizza <rickomo.us@gmail.com>
2024-04-14 19:54:28 +0200

    add nix dev flake and TODO.md

---
+++ TODO.md
@@ -0,0 +1,1 @@
+- [ ] add choice to decline/check before processing item 
---
+++ flake.nix
@@ -0,0 +1,29 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    #       ↑ Swap it for your system if needed
+    #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+
+      env = {};
+
+      packages = with pkgs; [ 
+        python312
+        python312Packages.cinemagoer
+      ];
+
+      shellHook = ''
+        echo "welcome!"
+      '';
+
+    };
+  };
+}
+
